cf2539108aca203ad278710670385a4945f3c76b
Isabela Montandon <imontandonbustamante@gmail.com>
2025-04-28 16:32:21 -0300

    Add files via upload

---
+++ função01.js
@@ -0,0 +1,14 @@
+function parimpar(n){
+    if(n%2 == 0){
+        return 'par'
+    } else {
+        return 'ímpar'
+    }
+}
+
+console.log(parimpar(5))
+
+ /*
+ let res = parimpar(4)
+ console.log(res)
+ */
---
+++ função02.js
@@ -0,0 +1,5 @@
+function soma( n1=0 , n2=0){ // =0 serve para quando não foi adicionado um valor
+    return n1 + n2
+}
+
+console.log(soma(7))
